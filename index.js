fd090417976577a8e84497f463305119107e32c0
amcc <alistairmcclymont@gmail.com>
2019-09-11 21:57:36 +0100

    some minor improvements

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// spawn_python.js
-// var util = require("util");
+// use python to do the grunt work
+// but node to do the smart stuff
 
 var spawn = require("child_process").spawn;
 var process = spawn("python", ["python.py"]);
 
-// util.log("readingin");
-
 process.stdout.on("data", function(chunk) {
   var textChunk = chunk.toString("utf8"); // buffer to string
+  
   // preserve newlines, etc - use valid JSON
   s = textChunk
     .replace(/\\n/g, "\\n")
@@ -20,11 +19,9 @@ process.stdout.on("data", function(chunk) {
     .replace(/\\f/g, "\\f");
   // remove non-printable and other non-valid JSON chars
   s = s.replace(/[\u0000-\u0019]+/g, "");
-  // console.log(textChunk);
-  // js read this
+
+  // turn it into JSON
   var json = JSON.parse(s);
 
   console.log(json);
-
-  // util.log(textChunk);
 });
